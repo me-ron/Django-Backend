56206ce82e3b150194b248a9b56ad69c9098f225
Nathnael Yirga <natty@localhost.localdomain>
2024-04-02 19:58:59 +0300

    minor bug fixes

--- CampusSync/event/urls.py
+++ CampusSync/event/urls.py
@@ -1,17 +1,4 @@
-from django.urls import path 
-from . import views
-from rest_framework.generics import ListCreateAPIView
-from .models import Event
-from .serializer import EventSerializer
 from .views import EventViewSet
-  
-# urlpatterns = [ 
-#     path('updateEvent/', views.update_event, name = 'update-event'), 
-#     path('deleteEvent/', views.delete_event, name = 'delete-event'), 
-#     path('events/', ListCreateAPIView.as_view(queryset=Event.objects.all(), serializer_class=EventSerializer), name='event-create-and-list')
-
-# ]
-
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -19,9 +6,3 @@ router.register(r'events', EventViewSet, basename='event')
 urlpatterns = router.urls
 
  
-urlpatterns = [ 
-    path('updateEvent/', views.update_event, name = 'update-event'), 
-    path('deleteEvent/', views.delete_event, name = 'delete-event'), 
-    path('events/', ListCreateAPIView.as_view(queryset=Event.objects.all(), serializer_class=EventSerializer), name='event-list')
-
-]
