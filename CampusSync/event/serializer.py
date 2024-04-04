36f52925c62931e3a33419608c8daa389c4aeadc
Nathnael Yirga <natty@localhost.localdomain>
2024-04-04 10:32:58 +0300

    serializer mods ...

--- CampusSync/event/serializer.py
+++ CampusSync/event/serializer.py
@@ -5,15 +5,26 @@ from user.models import Host
 
 
 class HostSerializer(serializers.ModelSerializer):
+    hostname = serializers.CharField(
+        read_only=True)
+    
     class Meta:
         model = Host
-        # fields = ['id', 'name', 'description', 'event_date'
-        #           , 'date_posted',  'poster', 'host', 'upvotes', 'downvotes', 'saved_by']
-
-        fields = ['id']
+        fields = ['id', 'hostname']
 
 class EventSerializer(serializers.ModelSerializer):
     host = serializers.PrimaryKeyRelatedField(queryset=Host.objects.all())
+    event_date = serializers.DateTimeField(read_only=False)
+    date_posted = serializers.DateTimeField(read_only=True)
+    poster = serializers.ImageField(read_only=False)
+    upvotes = serializers.IntegerField(read_only=True)
+    downvotes = serializers.IntegerField(read_only=True)
+    atendees = serializers.PrimaryKeyRelatedField( read_only=True)
+    saved_by = serializers.PrimaryKeyRelatedField( read_only=True)
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ['id', 'name', 'description'
+                  , 'event_date', 'date_posted', 'poster'
+                  , 'upvotes', 'downvotes', 'host', 'atendees'
+                  , 'saved_by']
