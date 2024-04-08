50c5d298d3c3d56cc056e6d7d37ae76e02e6cce4
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2024-04-08 15:25:11 +0300

    forum app views, urls, and more (#6)

---
+++ CampusSync/forum/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from .views import QuestionList, QuestionDetail, AnswerList, AnswerDetail
+
+urlpatterns = [
+    path('questions/', QuestionList.as_view(), name='question-list'),
+    path('questions/<int:pk>/', QuestionDetail.as_view(), name='question-detail'),
+    path('answers/', AnswerList.as_view(), name='answer-list'),
+    path('answers/<int:pk>/', AnswerDetail.as_view(), name='answer-detail'),
+    path('forum/', include('forum.urls')),
+]
--- CampusSync/forum/views.py
+++ CampusSync/forum/views.py
@@ -1,3 +1,85 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Question, Answer
+from .serializers import QuestionSerializer, AnswerSerializer
+from django.http import Http404
 
-# Create your views here.
+
+class QuestionList(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class QuestionDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Question.objects.get(pk=pk)
+        except Question.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        question = self.get_object(pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AnswerList(APIView):
+    def get(self, request):
+        answers = Answer.objects.all()
+        serializer = AnswerSerializer(answers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AnswerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AnswerDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Answer.objects.get(pk=pk)
+        except Answer.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        answer = self.get_object(pk)
+        serializer = AnswerSerializer(answer)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        answer = self.get_object(pk)
+        serializer = AnswerSerializer(answer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        answer = self.get_object(pk)
+        answer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
