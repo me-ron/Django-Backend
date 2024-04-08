6c4f14e4693dcd3b66210223bceb6fe120d171fe
Nathnael Yirga <natty@localhost.localdomain>
2024-04-08 23:50:35 +0300

    comp n req ..

--- CampusSync/CampusSync/settings.py
+++ CampusSync/CampusSync/settings.py
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
 }
 
 STATIC_URL = 'static/'
