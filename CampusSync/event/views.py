1cc3321ed1e6db8ad75eb2058a81894fc08a390c
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-04-10 10:33:11 +0300

    Upvote and Downvote for events (#11)
    Co-authored-by: Nathnael Yirga <103582061+Nxttyy@users.noreply.github.com>

--- CampusSync/event/views.py
+++ CampusSync/event/views.py
@@ -1,3 +1,13 @@
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import render
+from rest_framework.response import Response
+from .serializer import EventSerializer
+from .models import Event
+
+
+import mimetypes
+from django.http import HttpResponse
+
 from django.shortcuts import render, get_object_or_404
 
 from rest_framework.response import Response
@@ -7,8 +17,8 @@ from .models import Event, Comment
 from user.models import Host
 # from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
 from drf_spectacular.utils import extend_schema, OpenApiExample, inline_serializer
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework import status
 from rest_framework import generics
@@ -86,6 +96,50 @@ def custom_404(request, exception):
     print("$$")
     return render(request, 'event/404.html', status=404)
 
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def upvote_event(request, event_id):
+    try:
+        event = Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        return Response({'error': 'Event not found'}, status=404)
+
+    if request.method == 'POST':
+        user = request.user
+        if user in event.voters.all():
+            return Response({'error': 'You have already upvoted this event'}, status=400)
+        event.voters.add(user)
+        event.upvotes = event.voters.count()
+        event.save()
+
+        return Response({'status': 'Upvoted successfully',
+                         'event_id': event_id,
+                         'upvotes': event.upvotes})
+    else:
+        return Response({'error': 'Invalid request method'}, status=405)
+    
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+def downvote_event(request, event_id):
+    try:
+        event = Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        return Response({'error': 'Event not found'}, status=404)
+
+    if request.method == 'POST':
+        user = request.user
+        if user in event.downvoters.all():
+            return Response({'error': 'You have already downvoted this event'}, status=400)
+
+        event.downvoters.add(user)
+        event.downvotes = event.downvoters.count()
+        event.save()
+
+        return Response({'status': 'Downvoted successfully',
+                         'event_id': event_id,
+                         'downvotes': event.downvotes})
+    else:
+        return Response({'error': 'Invalid request method'}, status=405)
 
 
 @api_view(['GET'])
